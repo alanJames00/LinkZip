4deadd26f6683b93a0eb285b68b600c108df7f03
alanjames00 <alanjames6282@gmail.com>
2023-10-11 23:08:23 +0530

    refactor index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require('express');
 const apiRouter = require('./api/routes');
 const app = express();
@@ -10,9 +9,6 @@ app.use('/api', apiRouter);
 app.get('/',(req, res)=>{
     res.send('Hello, this is TinyTim')
 })
-
-
-
 
 app.listen(port, ()=>{
     console.log(`server listening on ${port}`);
