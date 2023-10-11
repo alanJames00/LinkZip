2357b5740a8a5aae662f3c2c90316e4cac173283
alanjames00 <alanjames6282@gmail.com>
2023-10-11 23:09:29 +0530

    setup env

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const apiRouter = require('./api/routes');
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT;
 
 app.use('/api', apiRouter);
 
 app.get('/',(req, res)=>{
-    res.send('Hello, this is TinyTim')
+    res.send('Hello, this is TinyTim');
 })
 
 app.listen(port, ()=>{
