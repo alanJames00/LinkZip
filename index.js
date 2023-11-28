455fa2191da4f53759122d31f2dd4cc190947825
alanjames00 <alanjames6282@gmail.com>
2023-11-28 17:25:37 +0530

    add api files

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const apiRouter = require('./api/routes');
 const app = express();
+const cors = require('cors')
 
 const port = process.env.PORT;
 
+app.use(cors());
 app.use('/', apiRouter);
 
 app.listen(port, ()=>{
